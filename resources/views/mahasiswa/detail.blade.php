f8c08151dcbc812d4c21613a0d118a981f8a03f1
Genadi Dharma <genadislawa@gmail.com>
2021-03-23 12:50:32 +0800

    tugas 2 - prev and next fixed

--- resources/views/mahasiswa/detail.blade.php
+++ resources/views/mahasiswa/detail.blade.php
@@ -21,7 +21,7 @@
             </div>
             <a class="btn btn-success mx-3" href="{{ route('mahasiswa.index') }}">Kembali</a>
             <div class="d-flex justify-content-between">
-                <a class="btn m-3 {{isset($prev->nim) ? 'btn-outline-primary' : 'disabled' }}" href="{{ isset($prev->nim) ? route('mahasiswa.show', $prev->nim) : '' }}"><i class="fas fa-chevron-left"></i> Sebelumnya</a>
+                <a class="btn m-3 {{isset($prev->nim) ? 'btn-outline-primary' : 'disabled' }}" href="{{ isset($prev->nim) ? route('mahasiswa.show', $prev->nim) : '' }}"><i class="fas fa-chevron-left"></i> Sebelumnya</i></a>
                 <a class="btn m-3 {{isset($next->nim) ? 'btn-outline-primary' : 'disabled' }}" href="{{ isset($next->nim) ? route('mahasiswa.show', $next->nim) : '' }}">Selanjutnya <i class="fas fa-chevron-right"></i></a>
             </div>
         </div>
