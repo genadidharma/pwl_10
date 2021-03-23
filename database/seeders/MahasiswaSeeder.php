cf1bde018139df8dcccb19418a63a2f1ef4924fe
Genadi Dharma <genadislawa@gmail.com>
2021-03-23 10:10:50 +0800

    prak 1 -MahasiswaSeeder filled and seeded

--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaSeeder extends Seeder
 {
@@ -13,6 +14,28 @@ class MahasiswaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('mahasiswa')->insert([
+            [
+                'nim' => '1941720070',
+                'nama' => 'I Made Genadi Dharma Slawa',
+                'kelas' => 'TI-2D',
+                'jurusan' => 'Teknologi Informasi',
+                'no_handphone' => '085238831025'
+            ],
+            [
+                'nim' => '1941720051',
+                'nama' => 'Gisanda Aliya',
+                'kelas' => 'TI-2B',
+                'jurusan' => 'Teknologi Informasi',
+                'no_handphone' => '081456743241'
+            ],
+            [
+                'nim' => '1941720121',
+                'nama' => 'Reynaldi Ramadhani',
+                'kelas' => 'TI-2D',
+                'jurusan' => 'Teknologi Informasi',
+                'no_handphone' => '084378652341'
+            ]
+        ]);
     }
 }
