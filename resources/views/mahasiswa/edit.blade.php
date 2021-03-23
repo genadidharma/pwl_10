d5f07b1a5c9a1899ed406e4fdfdcafdb058586f1
Genadi Dharma <genadislawa@gmail.com>
2021-03-23 10:40:34 +0800

    prak 1 - edit view created

---
+++ resources/views/mahasiswa/edit.blade.php
@@ -0,0 +1,50 @@
+@extends('mahasiswa.layout')
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center align-items-center">
+        <div class="card" style="width: 24rem;">
+            <div class="card-header">
+                Edit Mahasiswa
+            </div>
+            <div class="card-body">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your i
+                    nput.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+                <form method="post" action="{{ route('mahasiswa.update', $mahasiswa->nim) }}" id="myForm">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group">
+                        <label for="Nim">Nim</label>
+                        <input type="text" name="nim" class="form-control" id="Nim" value="{{ $mahasiswa->nim }}" aria- describedby="Nim">
+                    </div>
+                    <div class="form-group">
+                        <label for="Nama">Nama</label>
+                        <input type="text" name="nama" class="form-control" id="Nama" value="{{ $mahasiswa->nama }}" aria- describedby="Nama">
+                    </div>
+                    <div class="form-group">
+                        <label for="Kelas">Kelas</label>
+                        <input type="Kelas" name="kelas" class="form-control" id="Kelas" value="{{ $mahasiswa->kelas }}" aria- describedby="Kelas">
+                    </div>
+                    <div class="form-group">
+                        <label for="Jurusan">Jurusan</label>
+                        <input type="Jurusan" name="Jurusan" class="form-control" id="Jurusan" value="{{ $mahasiswa->jurusan }}" aria- describedby="Jurusan">
+                    </div>
+                    <div class="form-group">
+                        <label for="No_Handphone">No_Handphone</label>
+                        <input type="No_Handphone" name="no_handphone" class="form-control" id="No_Handphone" value="{{ $mahasiswa->no_handphone }}" aria- describedby="No_Handphone">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
